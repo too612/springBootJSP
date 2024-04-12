93ca60fd0d5ac64ab6d7563a6e5b098c578789b0
too612 <too612@naver.com>
2024-04-12 15:50:49 +0900

    first commit

---
+++ src/main/java/com/base/app/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.base.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/")
+	public ModelAndView index() throws Exception{
+		ModelAndView mav = new ModelAndView();
+		System.out.println("tttaaaaaatt");
+		mav.setViewName("index"); //jsp(html)로 갈때는 setViewName // class로 갈때는 setView
+		return mav;
+	}
+
+    @RequestMapping(value = "/main")
+	public ModelAndView main() throws Exception{
+		ModelAndView mav = new ModelAndView();
+		System.out.println("ttttt");
+		mav.setViewName("main/header"); //jsp(html)로 갈때는 setViewName // class로 갈때는 setView
+		return mav;
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    hell2222o!
+</body>
+</html>
